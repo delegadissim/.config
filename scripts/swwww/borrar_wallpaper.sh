9d632e103ee58cf0f2d87494fa0166956313e931
Delegadissim <alumas9513@ieselcaminas.org>
2024-01-20 12:53:59 +0100

    llevat uns quants wallpapers

--- scripts/swwww/borrar_wallpaper.sh
+++ scripts/swwww/borrar_wallpaper.sh
@@ -5,6 +5,8 @@ photo=$(swww query | awk '{print $NF}' | head -1)
 
 echo $photo
 ~/.config/scripts/swwww/change.sh ~/.config/wallpapers
+git rm $photo
 rm $photo
+
 
 notify-send "Alerta" "S'ha esborrat el fons d'escriptori $photo"
